882d5f1db62b0d9505a89098d177e3395fdd188e
darius <darius.jain@outlook.com>
2024-06-05 22:53:38 +0530

    commiting project

--- learning/excersizes2/parantha.py
+++ learning/excersizes2/parantha.py
@@ -1,44 +1,64 @@
-# create a class on parantha to to define properties and function
-"""
-create a list for paranthas
-    methods: consume, cooking and how many rotis
-    variables: color, fillng temparature
-"""
+import pprint 
+# dictionary and variable creation
+paranthas,paranthas_name_list,user_input={},[],''
+# defining class and functions
+class ParanthaClass():
 
-class Parantha():
-
-    paranthas=0
+    paranthaCount=0
     def __init__(self,color,filling,temprature) -> None:
-        self.color=color
-        self.filling=filling
-        self.temprature=temprature
-    def __str__(self) -> str:
-        return f'The {self.color} parantha filled with {self.filling} at the temprature {self.temprature}'
-    def cooking():
-        Parantha.paranthas+=1
-    def consume():
-        Parantha.paranthas-=1
-    def print_paranthas():
-        Parantha.paranthas
-        
+        self.color = color
+        self.filling = filling
+        self.temprature = temprature
+    def cook():
+        ParanthaClass.paranthaCount+=1
+        print('One parantha cooked.')
+    def eat():
+        ParanthaClass.paranthaCount-=1
+        print('One parantha eaten.')
+    def parantha_print():
+        print(ParanthaClass.paranthaCount)
+def print_inst():
+    print(f'''
+Hi, this is the instruction manual. this is a program designed to teach the developer about
+how to use functions and classes by making a program based on paranthas, an indian dish origionating
+from 1526 AD frequently enjoyed by  panjab.
+
+Here are a few different functions:
+help (gives you the commands)
+exit (quits program) 
+add (this creates a new variaty of paranthas)
+cook (entering this will provide you with a variety of paranthas you have made, enter number)
+eat (entering this will provide you with a variety of paranthas you have made, enter number)
+show (shows a singualar variaty of parantha)
+
+    ''')
 def main():
-    parantha_obj=Parantha(input('Color of parantha : ', input('Filling of parantha : '), input('Parantha temprature')))
-    print('')
-    x = True
-    while x == True:
-        userInput=input('Do you want to cook, eat or see how many paranthas are there or exit the program : ').lower()
-        if userInput == 'cook':
-            print('You have +1 parantha')
-            Parantha.cooking()
-        elif userInput == 'eat':
-            print('You have -1 parantha')
-            pass
-        elif userInput == 'exit':
-            exit(0)
+     while True:
+        user_input=input(' : ')
+        if user_input != 'exit':
+            if user_input == 'help':
+                print_inst()
+            elif user_input == 'add':
+                x = ParanthaClass(input('Enter parantha colour : '), input('Enter parantha filling : '),input('ENter parantha temprature : '))
+                paranthas_name_list.append(x.filling)
+                paranthas[{f'{x.filling}':{
+                    'color':x.color,
+                    'filling':x.filling,
+                    'temprature':x.temprature
+                }}]
+            elif user_input == 'show':
+                pprint.pprint(paranthas_name_list)
+            elif user_input == 'cook':
+                pprint.pprint(paranthas_name_list)
+                user_input=input(' : ')
+                for i in len(paranthas_name_list):
+                    if 
         else:
-            print('x')
-    
-if __name__== '__main__':
-    main() 
-else:
-    print('.................. why arent you WORKING !!!!!!!')
+            print('')
+            print('')
+            print('Yipee kayak other buckets !! - charles boyle')
+            exit(0)
+#running the program
+if __name__=='__main__':
+    print_inst()
+    main()
