ea536faf179450ee7dc78fd7e3b57120f4f798b9
darius <darius.jain@outlook.com>
2024-06-09 09:24:38 +0530

    Co-authored-by: Loveen <loveen@users.noreply.github.com>

---
+++ learning/excersizes2/banking.py
@@ -0,0 +1,12 @@
+# program on bank acounts for adding, removing, depositing, withdrawing 
+# var decs
+UserInput, BankAcounts = '',{}
+class BankAcount():
+    def __init__(self) -> None:
+        pass
+def running_program():
+    pass
+# running the program
+__name__ = 'running'
+if '__name__' == 'running':
+    running_program()
--- learning/excersizes2/parantha.py
+++ learning/excersizes2/parantha.py
@@ -1,64 +1,55 @@
-import pprint 
-# dictionary and variable creation
-paranthas,paranthas_name_list,user_input={},[],''
-# defining class and functions
+''''
+Enter command in the input
+
+'''
 class ParanthaClass():
-    paranthaCount=0
-    def __init__(self,color,filling,temprature) -> None:
-        self.color = color
+    def __init__(self,p_amount,cook,eat,filling) -> None:
+        self.p_amount = int(p_amount)
         self.filling = filling
-        self.temprature = temprature
-    def cook():
-        ParanthaClass.paranthaCount+=1
-        print('One parantha cooked.')
-    def eat():
-        ParanthaClass.paranthaCount-=1
-        print('One parantha eaten.')
-    def parantha_print():
-        print(ParanthaClass.paranthaCount)
-def print_inst():
+        self.cook=cook
+        self.eat=eat
+    def show_p(self):
+        print(f'parantha amount = {self.p_amount}')
+        print(f'filling = {self.filling}')
+    def eat_p(self):
+        self.p_amount-=1
+        print(f'A parantha eaten ...')
+    def cook_p(self):
+        self.p_amount+=1
+        print(f'A parantha cooked ...')
+
+        
+def print_manual():
     print(f'''
-Hi, this is the instruction manual. this is a program designed to teach the developer about
-how to use functions and classes by making a program based on paranthas, an indian dish origionating
-from 1526 AD frequently enjoyed by  panjab.
+Hello, this is a text simulator on eating, cooking and creating different varieties of paranthas
+here are the different instructions you have at your control.
+          - help ( gives you into and list of instructions )
+          - add ( creates new variant of paranthas)
+          - cook ( makes you cook a parantha of any variant)
+          - eat ( makes you cook a parantha of any variant)
+          - show ( shows parantha filling and how much paranthas are there )
+Enter the following commands in the input
+''')
+    
+def user_input(user_input_var,parantha_types,key_num):
+    key_num=0
+    if user_input_var != 'exit':
+        if user_input_var == 'help':
+            print_manual()
+        if user_input_var == 'add':
+            key_num+=1
+            parantha_types[str(key_num)]=ParanthaClass(0,input(''))
+        if user_input_var == 'show':
+            pass
+    else:
+        exit(0)
 
-Here are a few different functions:
-help (gives you the commands)
-exit (quits program) 
-add (this creates a new variaty of paranthas)
-cook (entering this will provide you with a variety of paranthas you have made, enter number)
-eat (entering this will provide you with a variety of paranthas you have made, enter number)
-show (shows a singualar variaty of parantha)
+def start_file_program():
+    print_manual()
+    while True:
+        user_input(input(f'''                         
+enter command : '''),{},0)
 
-    ''')
-def main():
-     while True:
-        user_input=input(' : ')
-        if user_input != 'exit':
-            if user_input == 'help':
-                print_inst()
-            elif user_input == 'add':
-                x = ParanthaClass(input('Enter parantha colour : '), input('Enter parantha filling : '),input('ENter parantha temprature : '))
-                paranthas_name_list.append(x.filling)
-                paranthas[{f'{x.filling}':{
-                    'color':x.color,
-                    'filling':x.filling,
-                    'temprature':x.temprature
-                }}]
-            elif user_input == 'show':
-                pprint.pprint(paranthas_name_list)
-            elif user_input == 'cook':
-                pprint.pprint(paranthas_name_list)
-                user_input=input(' : ')
-                for i in len(paranthas_name_list):
-                    if user_input == paranthas_name_list[i]:
-                         paranthas_name_list[i].paranthacount   
-        else:
-            print('')
-            print('')
-            print('Yipee kayak other buckets !! - charles boyle')
-            exit(0)
-#running the program
-if __name__=='__main__':
-    print_inst()
-    main()
+# Main execution of program
+if __name__ == "__main__":
+    start_file_program()
